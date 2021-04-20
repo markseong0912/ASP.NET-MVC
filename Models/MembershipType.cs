6091099aab2751e2d829fa9a8562d40a34224978
markseong0912 <82159875+markseong0912@users.noreply.github.com>
2021-04-20 10:13:17 +0900

    Getters and setters for models

---
+++ Models/MembershipType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewVidly.Models
+{
+    public class MembershipType 
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationsInMonth { get; set; }
+        public byte DiscountRate { get; set; }
+
+        public static string PayasYouGO;
+        public static string Monthly;
+    }
+}
