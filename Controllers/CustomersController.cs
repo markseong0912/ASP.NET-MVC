f3eb91d9c5d47d7f9a504e37cfe038f6790f67c9
markseong0912 <82159875+markseong0912@users.noreply.github.com>
2021-04-20 10:10:27 +0900

    Controllers' files

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewVidly.Models;
+using NewVidly.ViewModels;
+
+namespace NewVidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        private ApplicationDbContext _context;//getting the content from database
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ViewResult Index()
+        {
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customers == null)
+                return HttpNotFound();
+            return View(customers);
+        }
+
+        
+
+    }
+
+}
