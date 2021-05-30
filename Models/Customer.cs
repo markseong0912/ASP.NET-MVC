e76c2f7686f197fe9d52cb9669d73548ece3e177
markseong0912 <82159875+markseong0912@users.noreply.github.com>
2021-05-30 10:39:30 +0900

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; } //navigation property
+        //it allows to navigate from one type to another. (pro)->navigation property -> loading an object and its related objects together from a database
+        //[Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+        [Display(Name = "Date of Birth")]
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
