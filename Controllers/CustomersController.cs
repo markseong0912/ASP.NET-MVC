51fb88fdbcf3db08e14fb169f4eaaffdc0fc8947
markseong0912 <82159875+markseong0912@users.noreply.github.com>
2021-05-30 10:31:40 +0900

    Add files via upload
    Additional codes have been added.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+		// GET: Customers
+		private ApplicationDbContext _context;
+        public CustomersController()
+        {
+			_context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+			//base.Dispose(disposing);	
+			_context.Dispose();
+        }
+
+		public ActionResult New()
+        {
+			var membershipTypes = _context.MembershipTypes.ToList();
+			var viewModel = new NewCustomerViewModel
+			{
+				MembershipTypes = membershipTypes
+			};
+			return View();
+        }
+		public ViewResult Index()
+		{
+			var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+			return View(customers);
+		}
+		public ActionResult Details(int id)
+		{
+			var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+
+			if (customer == null)
+				return HttpNotFound();
+
+			return View(customer);
+		}
+		//      public ViewResult Index()
+		//{
+		//	var customers = GetCustomers();
+		//	return View(customers);
+		//}
+
+		//public ActionResult Details(int id)
+		//{
+		//	var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+		//	if (customer == null)
+		//		return HttpNotFound();
+		//	return View(customer);
+		//}
+
+		//private IEnumerable<Customer> GetCustomers()
+		//{
+		//	return new List<Customer>
+		//	{
+		//		new Customer {Id = 1, Name = "John Smith"},
+		//		new Customer {Id = 2, Name = "Mary Williams"}
+		//	};
+		//}
+	}
+}
